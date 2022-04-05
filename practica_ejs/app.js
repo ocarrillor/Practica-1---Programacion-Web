e113bf433491e908e17c43d5bfea78804962b196
ocarrillor <ocarrillo7@ucol.mx>
2022-04-05 18:00:53 -0500

    Practica con EJS

---
+++ practica_ejs/app.js
@@ -0,0 +1,30 @@
+var express = require('express'); //importamos la dependencia
+var app = express(); //declaramos una App de Express
+
+var port = process.env.PORT || 3000; //setteamos el puerto para que escuche el servidor
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.use('/', function (req, res, next) {
+    console.log('Request Url:' + req.url);
+    next();
+});
+
+//primera ruta (está al nivel de la raiz /), Hello World!
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+//segunda ruta, recibe un parametro
+app.get('/pairOdd/:id', function (req, res) {
+    res.render('pairOdd', { ID: req.params.id });
+});
+
+//tercera ruta /api, regresa un objeto JSON
+app.get('/api', function (req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port); //levantar el server y ponerlo a la escucha
